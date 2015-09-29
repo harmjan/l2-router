209a1c6cba69bb2be7c7e617f4e4443edc55bb47
Harmjan Treep <mail@harmjantreep.nl>
2015-09-29 13:23:47 +0200

    Add flow deletion before startup
    All flows must be added with cookie=1 to make
    it easy to detect old rules from this controller.

--- init.py
+++ init.py
@@ -21,19 +21,33 @@ class Init(app_manager.RyuApp):
         """
         new_datapath = ev.switch.dp
 
-        # Delete all previous flows
-        delete_msg = new_datapath.ofproto_parser.OFPFlowMod(
-                datapath=new_datapath,
-                command=new_datapath.ofproto.OFPFC_DELETE,
-                match=new_datapath.ofproto_parser.OFPMatch()
-        )
-        new_datapath.send_msg(delete_msg)
+
+        # Delete all previous flows in all tables, we can't
+        # just delete all flows since the topology discovery
+        # service also installs flows and runs before this
+        # module. Instead does this controller only add rules
+        # with cookie set to 1, which the following lines will
+        # delete.
+        for table in [0,1]:
+            delete_msg = new_datapath.ofproto_parser.OFPFlowMod(
+                    datapath=new_datapath,
+                    table_id=table,
+                    cookie=1,
+                    cookie_mask=1,
+                    command=new_datapath.ofproto.OFPFC_DELETE,
+                    out_port=new_datapath.ofproto.OFPP_ANY,
+                    out_group=new_datapath.ofproto.OFPG_ANY,
+                    match=new_datapath.ofproto_parser.OFPMatch(),
+                    instructions=[]
+            )
+            new_datapath.send_msg(delete_msg)
 
         # Add flowtable 0 table miss entry
         add_msg_0 = new_datapath.ofproto_parser.OFPFlowMod(
                 datapath=new_datapath,
                 table_id=0,
                 priority=0,
+                cookie=1,
                 command=new_datapath.ofproto.OFPFC_ADD,
                 match=new_datapath.ofproto_parser.OFPMatch(),
                 instructions=[
@@ -51,6 +65,7 @@ class Init(app_manager.RyuApp):
                 datapath=new_datapath,
                 table_id=1,
                 priority=0,
+                cookie=1,
                 command=new_datapath.ofproto.OFPFC_ADD,
                 match=new_datapath.ofproto_parser.OFPMatch(),
                 instructions=[
